dc07db7765f362d7be367a604825d856f8aabf9d
Francesco Fabozzi <fabozzi@cmsui04.na.infn.it>
2013-08-01 17:41:39 +0200

    cleanup

--- python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/HiggsAnalysis/Higgs2l2b/',1)[0])+'/cfipython/slc5_amd64_gcc462/HiggsAnalysis/Higgs2l2b')
